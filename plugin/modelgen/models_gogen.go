19d9a611b89900b8608b1aa91e53c7ad39720348
Felix Sun <sunfmin@gmail.com>
2019-05-25 21:00:41 +0800

    Add most of the models gen

--- plugin/modelgen/models_gogen.go
+++ plugin/modelgen/models_gogen.go
@@ -68,11 +68,63 @@ func (b *ModelsCodeBuilder) MarshalCode(ctx context.Context) (r []byte, err erro
 		for _, im := range m.Implements {
 			imps.Append(g.Block("func ($Rec) Is$Im() {}", "$Rec", fh.Go(m.Name), "$Im", fh.Go(im)))
 		}
-
 		root.Append(
 			g.LineComment(m.Description),
 			s,
 			imps,
+		)
+	}
+
+	for _, e := range b.mb.Enums {
+		typ := fh.Go(e.Name)
+		constVals := g.Codes().Separator(",\n", true)
+		cst := g.ConstBlock().Type(typ, "string")
+		for _, v := range e.Values {
+			name := fh.Go(v.Name)
+			cst.Consts(g.LineComment(v.Description))
+			cst.AppendConst(name, v.Name)
+
+			constVals.Append(g.Block("$Type$Val", "$Type", typ, "$Val", name))
+		}
+
+		root.Append(
+			g.LineComment(e.Description),
+			cst,
+			g.Block(`var All$Type = []$Type {
+					$Vals
+				}`, "$Type", typ).
+				VarBlock("$Vals", constVals),
+			g.Block(`func (e $Type) IsValid() bool {
+				switch e {
+				case $Vals:
+					return true
+				}
+				return false
+			}`, "$Type", typ).
+				VarBlock("$Vals", constVals.Clone().Separator(", ", false)),
+			g.Block(`
+func (e $Type) String() string {
+	return string(e)
+}
+
+func (e *$Type) UnmarshalGQL(v interface{}) error {
+	str, ok := v.(string)
+	if !ok {
+		return fmt.Errorf("enums must be strings")
+	}
+
+	*e = $Type(str)
+	if !e.IsValid() {
+		return fmt.Errorf("%s is not a valid $Name", str)
+	}
+	return nil
+}
+
+func (e $Type) MarshalGQL(w io.Writer) {
+	fmt.Fprint(w, strconv.Quote(e.String()))
+}
+
+			`, "$Type", typ, "$Name", e.Name),
 		)
 	}
 
